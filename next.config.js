2d09aed6c67c8308aac72019743083821f9ebe8b
muhammed-jasim <jasimkmails@gmail.com>
2025-06-26 01:48:38 +0530

    add next.config.js and update package.json for export script

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true, // optional but helpful for static hosting
+};
+
+module.exports = nextConfig;
